9d0e46d364ba05f2558cfe81bf3ffd562b18de9f
Shubham-5473 <bestintheworld14877@gmail.com>
2024-12-04 23:55:41 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { List, ListItem, ListItemText } from '@mui/material';
 
+const Sidebar = () => {
+  const menuItems = ['All Tasks', 'Completed Tasks', 'Pending Tasks', 'Overdue Tasks'];
+
+  return (
+    <List style={{ width: '250px', backgroundColor: '#1e293b', height: '100vh', color: 'white' }}>
+      {menuItems.map((item, index) => (
+        <ListItem button key={index}>
+          <ListItemText primary={item} />
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default Sidebar;
